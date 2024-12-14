565e4a9fb432a4fc6e507e5ba4eb3e1488184eb2
Vipul Singh <v.ipulsingh592005@gmail.com>
2024-12-14 19:15:50 +0530

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import mysql.connector
+
+def create_connection(Host,Username,Password,Database):
+    connection = None
+    try:
+        connection = mysql.connector.connect(host=Host,user= Username,password=Password,database=Database)
+        st.success("Connecte to Mysql Database Successfully")
+    except mysql.connector.Error as e:
+        st.error("You have entered something wrong! 😤")
+    
+    return connection
+    
+def main():
+    with st.form(key ='my_form'):
+        st.title("MySQL Connector")
+        Host = st.text_input("Host",placeholder='Host')
+        Username = st.text_input("Username",placeholder='Username') 
+        Password = st.text_input("Password",placeholder='Password',type='password')
+        Database = st.text_input("Database")
+        connect = st.form_submit_button(label='Connect it 🤝')
+        
+        if connect :
+            connection = create_connection(Host,Username,Password,Database)
+            if connection:
+                st.session_state['connection'] = connection
+                st.write("Connected Successfully")
+            else:
+                st.error("Connection Failed!",icon='🥲')
+                
+if __name__ =="__main__":
+    main()
+            
