f148627b404dbb4fa282ccb98971e819621cf5ba
Vipul Singh <v.ipulsingh592005@gmail.com>
2025-01-01 20:54:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
 import streamlit as st
 from importlib import import_module
-
-
-def mysql_connector_page():
-    # Implement logic for MySQL database connection here
-    st.write("This is the MySQL connector page.")
-
-
-def uploader_page():
-    # Implement file upload functionality here
-    st.write("This is the Uploader page.")
-
-
 def main():
-    pages = {
-        "MySQL Database Connection": mysql_connector_page,
-        "File Uploader": uploader_page,
-    }
 
-    st.sidebar.title("Navigation")
-    selected_page = st.sidebar.radio("Go to", list(pages.keys()))
-    selected_page()
+pages = {
+        "MySQL connector ⚙️ ": "connection",
+        
+        "Uploader 💻": "uploader",
+}
 
-if __name__ == "__main__":
-    main()
+
+
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to",list(pages.keys()))
+page = pages[selection]
+module = import_module(page)
+module.main()
+
+if __name__=="__main__":
+main()
