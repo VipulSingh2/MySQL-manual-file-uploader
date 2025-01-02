47d59f6c8c69dc2bb0582b5a0f96d12515495eac
Vipul Singh <v.ipulsingh592005@gmail.com>
2025-01-02 13:38:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import streamlit as st
 from importlib import import_module
 def main():
 
-pages = {
-        "MySQL connector ⚙️ ": "connection",
+        pages = {
+                "MySQL connector ⚙️ ": "connection",
+                
+                "Uploader 💻": "uploader",
+        }
         
-        "Uploader 💻": "uploader",
-}
-
-
-
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to",list(pages.keys()))
-page = pages[selection]
-module = import_module(page)
-module.main()
+        
+        
+        st.sidebar.title("Navigation")
+        selection = st.sidebar.radio("Go to",list(pages.keys()))
+        page = pages[selection]
+        module = import_module(page)
+        module.main()
 
 if __name__=="__main__":
 main()
