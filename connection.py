f62fd0d816efa7c19788864281e0cb87ef10ce6a
Vipul Singh <v.ipulsingh592005@gmail.com>
2025-01-02 13:53:33 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,34 +1,33 @@
 import streamlit as st
 import mysql.connector
 
-def create_connection(Host,Username,Password,Database):
+def create_connection(Host, Username, Password, Database):
     connection = None
     try:
-        connection = mysql.connector.connect(host=Host,user= Username,password=Password,database=Database)
-        st.success("Connecte to Mysql Database Successfully")
+        connection = mysql.connector.connect(host=Host, user=Username, password=Password, database=Database)
+        st.success("Connected to MySQL Database Successfully")
     except mysql.connector.Error as e:
         st.error("You have entered something wrong! 😤")
     
     return connection
     
 def main():
-    with st.form(key ='my_form'):
+    with st.form(key='my_form'):
         st.title("MySQL Connector")
-        Host = st.text_input("Host",placeholder='Host')
-        Username = st.text_input("Username",placeholder='Username') 
-        Password = st.text_input("Password",placeholder='Password',type='password')
+        Host = st.text_input("Host", placeholder='Host')
+        Username = st.text_input("Username", placeholder='Username') 
+        Password = st.text_input("Password", placeholder='Password', type='password')
         Database = st.text_input("Database")
         connect = st.form_submit_button(label='Connect it 🤝')
         
-        if connect :
+        if connect:
             if Host and Username and Password and Database:
-                connection = create_connection(Host,Username,Password,Database)
+                connection = create_connection(Host, Username, Password, Database)
             if connection:
                 st.session_state['connection'] = connection
                 st.write("Connected Successfully")
             else:
-                st.error("Connection Failed!",icon='🥲')
-                
-if __name__ =="__main__":
+                st.error("Connection Failed!", icon='🥲')
+
+if __name__ == "__main__":
     main()
-            
